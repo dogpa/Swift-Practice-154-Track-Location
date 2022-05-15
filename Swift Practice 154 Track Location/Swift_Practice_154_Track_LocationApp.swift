5188687965c46295b7d3d52982a4ac3ecce45643
Dogpa <dogpa0130@gmail.com>
2022-05-15 12:46:19 +0800

    Initial Commit

---
+++ Swift Practice 154 Track Location/Swift_Practice_154_Track_LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_154_Track_LocationApp.swift
+//  Swift Practice 154 Track Location
+//
+//  Created by Dogpa's MBAir M1 on 2022/5/15.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_154_Track_LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
